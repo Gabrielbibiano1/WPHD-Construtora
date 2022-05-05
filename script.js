39ab82920e5624d7f95776346cdedcf8af0d2d7b
Gabriel <gabrielbibiano1@hotmail.com>
2022-05-04 23:38:59 -0300

    Commit 1

---
+++ script.js
@@ -0,0 +1,24 @@
+function menuHomeAparece(){
+    var home = document.getElementById("home");
+    home.classList.toggle("ativo");
+}
+function menuNosAparece(){
+    var home = document.getElementById("nos");
+    home.classList.toggle("ativo");
+}
+function menuProjetosAparece(){
+    var home = document.getElementById("projetos");
+    home.classList.toggle("ativo");
+}
+function menuResidencialAparece(){
+    var home = document.getElementById("residencial");
+    home.classList.toggle("ativo");
+}
+function menuClientesAparece(){
+    var home = document.getElementById("clientes");
+    home.classList.toggle("ativo");
+}
+function menuContatoAparece(){
+    var home = document.getElementById("contato");
+    home.classList.toggle("ativo");
+}
